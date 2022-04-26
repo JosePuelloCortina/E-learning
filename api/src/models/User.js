61d7fea14ea0c04748de9dff02fc74ef0b9fa362
Jose Manuel Puello Cortina <puelloj66@gmail.com>
2022-04-25 23:31:16 -0500

    user

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -22,7 +22,7 @@ module.exports = (sequelize) => {
 
         password: {
             type: DataTypes.STRING,
-            allowNull: false,            
+            allowNull: false,             
         }, 
 
     })
