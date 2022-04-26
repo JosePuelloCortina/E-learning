f9189ad5523122c6bdf9bda01863be2b62b1cba1
Jose Manuel Puello Cortina <puelloj66@gmail.com>
2022-04-25 23:53:12 -0500

    tabla role

---
+++ api/src/models/Role.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('role', {
+        id :{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            unique: true,
+            primaryKey: true,
+            allowNull: false, 
+        },
+
+        tipo: {
+            type: DataTypes.ENUM("admin", "alumno", "instrcutor"),
+            allowNull: false,
+        }
+
+    })
+}
