b10154282b6434d1ec80b0c6a92fbe9a76baf459
ahubacek <hubacekk@gmail.com>
2022-04-26 16:46:54 -0300

    estilando landingPage, importando fuente

--- client/src/Componentes/LandingPage/LandingPage.jsx
+++ client/src/Componentes/LandingPage/LandingPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import styles from './landingPage.module.css'
 
 export default function LandingPage (){
     return (
-        <div>
-            <div className='button'>
-            <Link to="/home">Comienza ahora!</Link>
+        <div className={styles.container}>
+            <div className={styles.text}>
+            <h1>NOMBRE</h1>
+            <h2>Encontrá todos tus cursos IT acá</h2>
             </div>
+            <div>
+                <Link to="/home">
+                <button className={styles.button}>Comenzar ahora!</button>
+                </Link>
+            </div>
+        
             
         </div>
     )
