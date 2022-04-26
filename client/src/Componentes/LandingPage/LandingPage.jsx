c91445fe9fea83b384c0d7e01200f06019806f0f
ahubacek <hubacekk@gmail.com>
2022-04-26 18:46:59 -0300

    landing

--- client/src/Componentes/LandingPage/LandingPage.jsx
+++ client/src/Componentes/LandingPage/LandingPage.jsx
@@ -5,14 +5,16 @@ import styles from './landingPage.module.css'
 export default function LandingPage (){
     return (
         <div className={styles.container}>
-            <div className={styles.titulo}>
-                <h1>TITULO</h1>
+            <div className={styles.text}>
+            <h1>NOMBRE</h1>
+            <h2>Encontrá todos tus cursos IT acá</h2>
             </div>
-            <div >
-            <Link to="/home">
-            <button className={styles.button}>Comienza ahora!</button>
-            </Link>
+            <div>
+                <Link to="/home">
+                <button className={styles.button}>Comenzar ahora!</button>
+                </Link>
             </div>
+        
             
         </div>
     )
