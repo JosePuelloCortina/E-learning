9b8db135af953383ef0cc581b50e18a88a6c0157
ahubacek <hubacekk@gmail.com>
2022-04-26 15:24:04 -0300

    landingPage

--- client/src/Componentes/LandingPage/LandingPage.jsx
+++ client/src/Componentes/LandingPage/LandingPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import styles from './landingPage.module.css'
 
 export default function LandingPage (){
     return (
-        <div>
-            <div className='button'>
-            <Link to="/home">Comienza ahora!</Link>
+        <div className={styles.container}>
+            <div className={styles.titulo}>
+                <h1>TITULO</h1>
+            </div>
+            <div >
+            <Link to="/home">
+            <button className={styles.button}>Comienza ahora!</button>
+            </Link>
             </div>
             
         </div>
