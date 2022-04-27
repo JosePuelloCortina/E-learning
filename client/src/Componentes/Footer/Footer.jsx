a503555387f55534927f48ce1c5b839d6a060da3
albanovigna <anvignaduzzi@gmail.com>
2022-04-27 12:59:07 -0300

    footer styles

--- client/src/Componentes/Footer/Footer.jsx
+++ client/src/Componentes/Footer/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import styles from "./Footer.module.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.footerLeft}>
+        <div className={styles.footerLinks}>
+          <a className={styles.link} href="#">
+            Sobre Nosotros
+          </a>
+          <a className={styles.link} href="#">
+            Contacto
+          </a>
+        </div>
+
+        {/* <p className={styles.footerCompanyName}>grupo 3 &copy; 2022</p> */}
+      </div>
+
+      <div className={styles.footerRight}>
+        <div className={styles.footerIcons}>
+          <a href="https://www.linkedin.com/" target="_blank">
+            <img
+              src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+              alt="Png Linkedin Icon"
+            />
+          </a>
+          <a
+            href="https://github.com/JosePuelloCortina/E-learning"
+            target="_blank"
+          >
+            <img
+              src="https://img.icons8.com/ios-glyphs/344/github.png"
+              width="350"
+              alt="Png Github Icon"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
