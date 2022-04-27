81132fd3006f224dc3482724595b83b53232c812
ahubacek <hubacekk@gmail.com>
2022-04-27 02:28:00 -0300

    añadiendo nombre, imagen y boton para editar en la barra lateral de perfil

--- client/src/Componentes/ProfileLateralBar/ProfileLateralBar.jsx
+++ client/src/Componentes/ProfileLateralBar/ProfileLateralBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from './profileLateralBar.module.css'
+import {Link} from 'react-router-dom'
+
 
 export default function profileLateralBar() {
+    
     return(
         <div className={styles.container}>
-        Barra Lateral Perfil
+        
+        <h3>Andrea Hubacek</h3>
 
+        <img src='.../Images.avatar1.jpg' alt='avatar' />
+        <Link to="/editProfile">
+        <button className={styles.editButton}>Editar mis datos</button>
+        </Link>
         </div>
     )
 }
