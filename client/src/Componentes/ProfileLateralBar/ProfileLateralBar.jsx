1affbf2c688b38e653d09f0e30e891df06fc3253
ahubacek <hubacekk@gmail.com>
2022-04-27 15:50:05 -0300

    props profile

--- client/src/Componentes/ProfileLateralBar/ProfileLateralBar.jsx
+++ client/src/Componentes/ProfileLateralBar/ProfileLateralBar.jsx
@@ -7,15 +7,14 @@ import img from '../../Images/avatar1.jpg'
 
 export default function ProfileLateralBar(user) {
     
-
     return(
         <div className={styles.container}>
         
-        <h3>{user.name}</h3>
+        <h3>{user[0].name}</h3>
 
         <img src={img} alt='avatar' />
-        <h4>{user.email}</h4>
-        <Link to={"/profile/edit/"+ user.id}>
+        <h4>{user[0].email}</h4>
+        <Link to={"/profile/edit/"+ user[0].id}>
         <button className={styles.editButton}>Editar mis datos</button>
         </Link>
         </div>
