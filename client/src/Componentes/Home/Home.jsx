d3b9ddb3bcfe77fbbba94708763b22508cb34db6
albanovigna <anvignaduzzi@gmail.com>
2022-04-27 17:01:32 -0300

    Merge branch 'main' of https://github.com/JosePuelloCortina/E-learning into footer

--- client/src/Componentes/Home/Home.jsx
+++ client/src/Componentes/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import CoursesContainer from "../CoursesContainer/CoursesContainer";
+import Pagination from "../Pagination/Pagination";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <h1>Home </h1>
-      <CoursesContainer/>
+      <CoursesContainer />
+      <Pagination />
       <Footer />
     </div>
   );
