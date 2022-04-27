0bc4dba0d7bf77aa61950795217cbd65fdc90eaf
albanovigna <anvignaduzzi@gmail.com>
2022-04-26 23:22:33 -0300

    added footer in home

--- client/src/Componentes/Home/Home.jsx
+++ client/src/Componentes/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import NavBar from '../NavBar/NavBar'
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
 
-export default function Home(){
-
-    return(
-        <div>
-            <NavBar/>
-            <h1>Home </h1>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home </h1>
+      <Footer />
+    </div>
+  );
 }
