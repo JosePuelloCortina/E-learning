d6ec88abc49b4c24d18d71b5a8d301c5751e7c07
ahubacek <hubacekk@gmail.com>
2022-04-27 00:23:01 -0300

    creando pagina perfil, incluyendo navbar y footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useMemo, useEffect } from 'react';
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Home from './Componentes/Home/Home';
 import LandingPage from './Componentes/LandingPage/LandingPage';
+import UserProfile from './Componentes/UserProfile/UserProfile';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route exact path = '/' element={<LandingPage/>} />
             <Route path = '/home' element={<Home/>} />
+            <Route path = '/profile' element={<UserProfile/>} />
           </Routes>
         </BrowserRouter>
     </div>
