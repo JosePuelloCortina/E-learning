652650de5047bdf628340fb7889308ef530ec14f
albanovigna <anvignaduzzi@gmail.com>
2022-04-28 12:07:22 -0300

    logica card detail

--- client/src/Componentes/CourseCard/CourseCard.jsx
+++ client/src/Componentes/CourseCard/CourseCard.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import styles from './CourseCard.module.css'
+import styles from "./CourseCard.module.css";
+import { Link } from "react-router-dom";
 
-export default function CourseCard({name, img}) {
-
-    return (
-        <div className={styles.card}>
-            <div className={styles.image}>
-                <img src={img} alt="" />
-            </div>
-            <div className={styles.info}>
-                <div >
-                    <p><strong>{name}</strong></p>
-                    <p>Categorias</p>
-                </div>
-                <div >
-                    <p>4⭐</p>
-                    <p><strong>$20</strong></p>
-                </div>
-            </div>
+export default function CourseCard({ name, img, id }) {
+  return (
+    <Link className={styles.linkDetail} to={`/courses/id/${id}`}>
+      <div className={styles.card}>
+        <div className={styles.image}>
+          <img src={img} alt="" />
         </div>
-    )
+        <div className={styles.info}>
+          <div>
+            <p>
+              <strong>{name}</strong>
+            </p>
+            <p>Categorias</p>
+          </div>
+          <div>
+            <p>4⭐</p>
+            <p>
+              <strong>$20</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 }
--- client/src/Componentes/CoursesContainer/CoursesContainer.jsx
+++ client/src/Componentes/CoursesContainer/CoursesContainer.jsx
@@ -5,23 +5,10 @@ import CourseCard from "../CourseCard/CourseCard";
 import styles from "./CoursesContainer.module.css";
 
 export default function CoursesContainer({ currentCourses }) {
-  // const dispatch = useDispatch()
-
-  // async function getCourses() {
-  //     await dispatch(allCourses())
-  // }
-
-  // useEffect(() => {
-  //     getCourses()
-  // }, [])
-
-  // const courses = useSelector(state => state.courses)
-  // console.log(courses)
-
   return (
     <div className={styles.container}>
       {currentCourses?.map((e) => (
-        <CourseCard name={e.name} img={e.img} />
+        <CourseCard key={e.id} name={e.name} img={e.img} id={e.id} />
       ))}
     </div>
   );
