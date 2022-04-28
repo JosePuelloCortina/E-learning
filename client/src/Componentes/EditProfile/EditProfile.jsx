dd0fc8a807e91e0ba366c474932cb85624d2fad4
ahubacek <hubacekk@gmail.com>
2022-04-27 23:06:53 -0300

    actualizar datos de perfil

--- client/src/Componentes/EditProfile/EditProfile.jsx
+++ client/src/Componentes/EditProfile/EditProfile.jsx
@@ -1,48 +1,95 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import { useDispatch, useSelector} from "react-redux";
 import styles from './editProfile.module.css'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
+import { getUserById, updateUser } from '../../redux/actions/index';
+import { useParams} from 'react-router-dom'
 
 
 
-export default function editProfile() {
-
+export default function EditProfile() {
+    const dispatch = useDispatch();
+    const {id} = useParams()
+    useEffect(()=> {dispatch(getUserById(id))},[dispatch, id])
+    const userInit = useSelector(state => state.user)
+    const navigate = useNavigate()
     
+    const[ input, setInput] = useState({
+        id:id,
+        name:userInit.name,
+        password:undefined,
+        confirmPassword: undefined,
+        email:userInit.email
+    })
+
+    console.log(input)
+    
+    function handleInputChange(e){
+    setInput({
+        ...input,
+        [e.target.name]: e.target.value
+    })
+}
+
+function handleSubmit(e){
+    dispatch(updateUser(id, input))
+    alert("Cambios guardados.")
+    navigate(`/profile/${id}`);
+    }
+
     return(
         <div>
         <NavBar/>
         <div className={styles.container}>
         <div><h2>Mis Datos</h2></div>
             <div className={styles.editPanel}>
-
+            <form onSubmit={handleSubmit}>
             <div className={styles.cont2}>
             <div className={styles.left}>
                 <div>
                     <label>Nombre y Apellido</label>
-                    <input type="text"/>
+                    <input type="text" 
+                            value={input.name} 
+                            onChange={handleInputChange}
+                            name="name"
+                            />
                 </div>
                 <div>
                     <label>Correo electrónico</label>
-                    <input type="text"/>
+                    <input type="text" 
+                            value={input.email} 
+                            onChange={handleInputChange} 
+                            name="email"
+                            />
                 </div>
             </div>
 
             <div className={styles.right}>
                 <div>
                     <label>Nueva Contraseña</label>
-                    <input type="password" />
+                    <input type="password" 
+                            value={input.password} 
+                            onChange={handleInputChange}
+                            name="password"
+                    />
                 </div>
                 <div>
                     <label>Confirmar Contraseña</label>
-                    <input type="password"/>
+                    <input type="password" 
+                            value={input.confirmPassword}
+                            onChange={handleInputChange}
+                            name="confirmPassword"
+                            />
                 </div>
                 
                 </div>
                 </div>
                 <div>
-                <button className={styles.save}>Guardar Cambios</button>
+                <button type="submit" className={styles.save}>Guardar Cambios</button>
                 </div>
+                </form>
             </div>
     
         </div>
