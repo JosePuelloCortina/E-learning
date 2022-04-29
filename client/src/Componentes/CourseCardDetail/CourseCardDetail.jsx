9ed38adbaec9f9fec8941240867fdef3fd273a42
albanovigna <anvignaduzzi@gmail.com>
2022-04-29 17:16:59 -0300

    course detail

--- client/src/Componentes/CourseCardDetail/CourseCardDetail.jsx
+++ client/src/Componentes/CourseCardDetail/CourseCardDetail.jsx
@@ -7,6 +7,7 @@ import {
   removeCourseDetail,
 } from "../../redux/actions";
 import styles from "./CourseCardDetail.module.css";
+import { Link } from "react-router-dom";
 
 function CourseCardDetail() {
   const dispatch = useDispatch();
@@ -28,15 +29,37 @@ function CourseCardDetail() {
   return (
     <div>
       {detail ? (
-        <div>
-          <h3>{detail.name}</h3>
-          <h4>Instructor: {course[0].users[0].name}</h4>
-          <h4>{detail.description}</h4>
-          <h4>{detail.duration}</h4>
-          <h4>{detail.progress}</h4>
-          <h4>{detail.review}</h4>
-          <h4>{detail.image}</h4>
-          <h4>{detail.price}</h4>
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <div className={styles.image}>
+              <img src={detail.image} alt="" />
+            </div>
+            <div className={styles.courseInfo}>
+              <h3>{detail.name}</h3>
+              <h4>Instructor: {course[0].users[0].name}</h4>
+              <h4>{detail.description}</h4>
+              <h4>{detail.duration}</h4>
+              <h4>{detail.progress}</h4>
+              <h4>{detail.review}</h4>
+              <h4>{detail.price}</h4>
+            </div>
+
+            {detail.clases &&
+              detail.clases.map((e) => {
+                return (
+                  <div className={styles.containerClases}>
+                    <ul>
+                      <li>{e.name}</li>
+                      <li>{e.duration}</li>
+                      <li>{e.description}</li>
+                    </ul>
+                  </div>
+                );
+              })}
+            <Link to="/home">
+              <button>Go to home</button>
+            </Link>
+          </div>
         </div>
       ) : (
         <div>...Loading</div>
