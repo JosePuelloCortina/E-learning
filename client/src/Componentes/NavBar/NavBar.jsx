32ca45202af8ad83858736dc90725e651b798e6e
ahubacek <hubacekk@gmail.com>
2022-04-29 19:41:35 -0300

    estilos navbar

--- client/src/Componentes/NavBar/NavBar.jsx
+++ client/src/Componentes/NavBar/NavBar.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
 import SearchCourse from "../Search/SearchCourse";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const logedUser = useSelector(state =>state.loged )
 
         return (
     <div className={styles.navBar}>
       <Link className={styles.linkHome} to="/home">
-        <h1>Logo</h1>
+        <h1>AkademIT</h1>
       </Link>
       <SearchCourse />
       
       <Link to="/user">
-        <button>Ingresar</button>{" "}
+        <button>Mi perfil</button>
       </Link>
+      { !logedUser?
+      <Link to="/user">
+        <button>Ingresar</button>
+      </Link>
+    :<Link to=" ">
+        <button>Salir</button>
+      </Link>}
     </div>
   
   );
