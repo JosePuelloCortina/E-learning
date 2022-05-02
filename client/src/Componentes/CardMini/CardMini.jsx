ad4957dc7c27c49777b3704e1bc9dcf3d426f90a
Tom <tomaslm_3@hotmail.co>
2022-05-01 21:23:09 -0300

    Buen trabajo! <3

--- client/src/Componentes/CardMini/CardMini.jsx
+++ client/src/Componentes/CardMini/CardMini.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './cardMini.module.css'
 
 export default function CardMini ({name}){
+    console.log(name, "name")
     return(
         <div className={styles.container}>
         <h4>{name}</h4>
