19a824421a0b5ee76f6bb95c9156835154ecdaa9
albanovigna <anvignaduzzi@gmail.com>
2022-05-01 23:18:33 -0300

    searchBar fixed

--- client/src/Componentes/Search/SearchCourse.jsx
+++ client/src/Componentes/Search/SearchCourse.jsx
@@ -1,39 +1,56 @@
-import React from 'react';
+import React from "react";
 // import styles from "../NavBar/NavBar.module.css";
-import {useState} from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import {courseSearch} from "../../redux/actions/index"
+import { allCourses, courseSearch } from "../../redux/actions/index";
+import styles from "./SearchCourse.module.css";
 
-function SearchCourse() {
-    const dispatch = useDispatch()
-    const[name, setName] = useState ("")
+function SearchCourse({ setCurrentPage }) {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
 
-    function handleInputChange(e){
-e.preventDefault()
-setName(e.target.value)
-    }
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
 
-    function handleSubmitCourse(e){
-        e.preventDefault()
-        dispatch(courseSearch(name))
-        setName("")
-    } 
+  function handleSubmitCourse(e) {
+    e.preventDefault();
+    dispatch(courseSearch(name));
+    setCurrentPage(1);
+    // setName("");
+  }
 
-    function handleKeyPress(e){
-        dispatch(courseSearch(name))
-    }
-    return (
-        <div>
-            <input type = "text" 
-            placeholder="Buscar ..." 
-            value = {name} 
-            onChange={(e) =>handleInputChange(e)}
-            onKeyDown={(e) => handleKeyPress(e)}
-            />
-            <button type="submit" onClick={handleSubmitCourse}>Search </button>
-      
-        </div>
-    );
+  const handleReset = (e) => {
+    e.preventDefault();
+    dispatch(allCourses());
+    setName("");
+    setCurrentPage(1);
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        type="text"
+        placeholder="Buscar ..."
+        value={name}
+        onChange={(e) => handleInputChange(e)}
+      />
+      {name.length > 0 && (
+        <button
+          className={styles.resetButton}
+          //   style={{ background: "silver", color: "black" }}
+          onClick={(e) => handleReset(e)}
+        >
+          X
+        </button>
+      )}
+
+      <button type="submit" onClick={handleSubmitCourse}>
+        Search{" "}
+      </button>
+    </div>
+  );
 }
 
 export default SearchCourse;
