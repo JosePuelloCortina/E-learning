9f4d4d74225f757abd5270d035ec78e21fee3613
albanovigna <anvignaduzzi@gmail.com>
2022-05-03 18:08:33 -0300

    mis cursos instructor fixed

--- client/src/Componentes/MyCoursesAlumn/MyCoursesAlumn.jsx
+++ client/src/Componentes/MyCoursesAlumn/MyCoursesAlumn.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
-import styles from './myCoursesAlumn.module.css'
-import CardMini from '../CardMini/CardMini'
-import {Link} from 'react-router-dom'
-import { useSelector} from "react-redux";
-
+import React from "react";
+import styles from "./myCoursesAlumn.module.css";
+import CardMini from "../CardMini/CardMini";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function MyCoursesAlumn() {
-    
-    const user = useSelector(state => state.user)
-    const courses = useSelector(state => state.courses)
-    let myBuys = user.buys && user.buys.map(buy => buy.courseId)
-    let myCourses = courses.filter(course => myBuys.includes(course.id))
-    console.log(myCourses, "myCourses")
+  const user = useSelector((state) => state.user);
+  const courses = useSelector((state) => state.coursesBackUp);
+  let myBuys = user.buys ? user.buys.map((buy) => buy.courseId) : [];
+  let myCourses = courses.filter((course) => myBuys.includes(course.id));
+  console.log(myCourses, "myCourses");
 
-    return(
-        <div className={styles.container}>
-        <div className={styles.title}>
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
         <h2>Mis Cursos | Alumno |</h2>
-        </div>
-        <div className={styles.curses}>
-        {myCourses && myCourses.map(e => {
-            return(
-                <Link to={`/courselessons/${e.courseId}`}>
+      </div>
+      <div className={styles.curses}>
+        {myCourses &&
+          myCourses.map((e) => {
+            return (
+              <Link to={`/courselessons/${e.courseId}`}>
                 <CardMini key={e.id} name={e.name} />
-                </Link>
-            )
-        })}
-
-        </div>
-        </div>
-    )
+              </Link>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
