11b0ac97f94b9f30830ddb77b0f169172110db57
ahubacek <hubacekk@gmail.com>
2022-05-04 11:39:09 -0300

    creo componente para dejar reseña

--- client/src/Componentes/LessonsList/LessonsList.js
+++ client/src/Componentes/LessonsList/LessonsList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './lessonsList.module.css'
+import {Link} from 'react-router-dom'
 
-export default function LessonsList({lessons}){
+export default function LessonsList({lessons, review, setReview}){
     console.log(lessons, 'esto es lessons')
 
-    function handleClick(e){
+    function handleReview(e){
         e.preventDefault(e);
+        setReview(true)
         
     }
 
@@ -21,7 +23,10 @@ export default function LessonsList({lessons}){
                 
             )
         }): null}
-
+        <br/>
+        <h5>Tus comentarios nos ayudan a mejorar y ayudan a otros usuarios a elegir mejor.</h5> 
+        <h5>Deja tu reseña sobre este curso.</h5>
+        <h4 onClick={handleReview}>Click aquí para calificar.</h4>
         </div>
     )
 }
