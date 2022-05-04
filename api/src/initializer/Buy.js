78c35056396f27e8d5c0caf09f7b202bf85b2956
AgusAgis <89985580+AgusAgis@users.noreply.github.com>
2022-05-04 16:00:00 -0300

    mp

--- api/src/initializer/Buy.js
+++ api/src/initializer/Buy.js
@@ -10,26 +10,6 @@ let buys = [
         "courseId": "760c7440-c70e-11ec-96f7-e913400288b4",
         "courseName": "Fullstack Web Developer",
         "quantity" : 1
-    },
-    {
-        "id": "394606a0-c77e-11ec-8c73-834ec4650dd4",
-        "discount": 2,
-        "pay_method": "efectivo",
-        "total_price": 100,
-        "userId": "b65232b0-c6a0-11ec-b926-ff69e9773726",
-        "courseId": "760c7440-c70e-11ec-96f7-e913400288b5",
-        "courseName": "Software Developer",
-        "quantity" : 1
-    },
-    {
-        "id": "394606a0-c77e-11ec-8c73-834ec4650dd5",
-        "discount": 2,
-        "pay_method": "efectivo",
-        "total_price": 110,
-        "userId": "b65232b0-c6a0-11ec-b926-ff69e9773726",
-        "courseId": "760c7440-c70e-11ec-96f7-e913400288b6",
-        "courseName": "Data Science",
-        "quantity" : 1
     }
 ]
 
