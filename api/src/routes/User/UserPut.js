3c2867ef41caeb3f2e45858ea80ea4660ca8baee
Tom <tomaslm_3@hotmail.co>
2022-05-04 19:15:56 -0300

    El usuario ahora puede editar sus intereses desde editar usuario

--- api/src/routes/User/UserPut.js
+++ api/src/routes/User/UserPut.js
@@ -1,9 +1,9 @@
 const server = require('express').Router();
-const { User } = require('../../db');
+const { User, Category } = require('../../db');
 
 server.put('/update/id/:id', (req, res)=>{
     const { id } = req.params;
-    const { name, password, email } = req.body;
+    const { name, password, email, categories } = req.body;
     if(!name || !password || !email ){
         return res.status(422).json({error: "No se enviaron todos los datos"}) 
     }
@@ -13,6 +13,12 @@ server.put('/update/id/:id', (req, res)=>{
         name: name,
         password: password,
         email: email,
+    })
+    .then((userUpdate) =>{
+        userUpdate.setCategories(categories)
+        .then(async() =>{
+            userUpdate.categories = await userUpdate.getCategories()
+        })
     }))
     }).catch((error)=>{
         console.log(error)
