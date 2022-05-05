59c86d0f320497510f17bdd8c0ad38d65ee09797
Tom <tomaslm_3@hotmail.co>
2022-05-05 16:28:33 -0300

    Se puede elegir la clase dentro del curso

--- client/src/Componentes/LessonsVideo/LessonsVideo.js
+++ client/src/Componentes/LessonsVideo/LessonsVideo.js
@@ -3,22 +3,38 @@ import styles from './lessonsVideo.module.css'
 import video from '../../Images/videoClase.mp4'
 import ReactPlayer from 'react-player'
 
-export default function LessonsVideo({lessons}){
-    console.log(lessons, 'esto es lessons en video')
+export default function LessonsVideo({currentLesson, course}){
+    console.log(course, 'esto es course')
+    const {name, description, duration, url} = currentLesson
 
+    if(!currentLesson.name){
+        return (
+            <div className={styles.container}>
+            <div className={styles.up}>
+                <h2>Bienvenido a mi curso {course.name}</h2>
+                <img src={course.image} alt={course.name}/>
+                <h2>{course.description}</h2>
+            </div>
+            <div className={styles.down}>
+            </div>
+        </div>
+        )
+    }
     return(
         <div className={styles.container}>
             <div className={styles.up}>
-                <h2>Nombre de la clase</h2>
+                
+                <h2>{name}</h2>
                 {/* <video src={video} controls="true" /> */}
                 <ReactPlayer className={styles.video}
-                    url='https://www.youtube.com/watch?v=ivdTnPl1ND0'
+                    url={url}
                     controls='true'
                     
                 />
             </div>
             <div className={styles.down}>
-                <h2>Contenido escrito</h2>
+                <h2>{description}</h2>
+                <h3>{duration}</h3>
             </div>
         </div>
     )
