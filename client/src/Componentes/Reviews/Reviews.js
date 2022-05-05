b496c45910bbfbda40255ee73863215b837947bd
ahubacek <hubacekk@gmail.com>
2022-05-05 00:54:07 -0300

    inicio componente Reviews

---
+++ client/src/Componentes/Reviews/Reviews.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './reviews/reviews.module.css'
+
+export default function Reviews(){
+    
+
+    return(
+        <div className={styles.container}>
+            <h3>Reseñas sobre este curso</h3>
+            <div>
+
+            </div>
+        </div>
+    )
+}
