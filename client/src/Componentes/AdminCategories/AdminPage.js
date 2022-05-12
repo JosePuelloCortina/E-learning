8c722ae4fd99b6a029489a615771f856fb0348f5
valentinaavendanio <valentinaaven02@gmail.com>
2022-05-12 17:19:54 -0300

    diseño administrar categoria

--- client/src/Componentes/AdminCategories/AdminPage.js
+++ client/src/Componentes/AdminCategories/AdminPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { createCategory, allCategories, removeCategory } from "../../redux/actions/index";
 import { useDispatch, useSelector } from "react-redux";
 import styles from './AdminPage.module.css'
+import NavBar from "../NavBar/NavBar";
 
 function AgregarCategorias() {
   const dispatch = useDispatch();
@@ -31,32 +32,44 @@ function handleRemove(e){
   
   }
   return (
-    <div className={styles.containertotal}>
-      <div className={styles.containerSelect}>
-        {/* <label>Categoria</label> */}
-        <div className={styles.select}>
-        <ul className={styles.selecttodo} >
-          <h1 className={styles.h2}>Categorias</h1>
-          {categories.map((category) => { 
-            return (
-              <li key={category.name} value={category.name}>
-                {category.name}
-                <button value={category.id} onClick={(e) =>handleRemove(e)}>X</button>
-              </li>
-            );
-          })}
-        </ul>
-        <div className={styles.containertotal}>  
+    <div>
+      <NavBar />
+      <h1 className={styles.titulocategoria}>Administar Categorias</h1>
+      <table className={styles.table} border="1">
+        <tbody>
+          <tr>
+            <th width="10%">Categorias</th>
+            <th width="10%">Eliminar Categorias</th>
+          </tr>
       <div>
-      <input 
-      type="text" value= {input} name="name"
-      onChange={(e) =>handleChange(e)}></input>
-      <button onClick={(e) =>handleSubmit(e)}type="submit">Añadir categoria</button>
-     
-        </div>             
-      </div>
+        <div>
+            {categories.map((category) => {
+              return (
+                <li key={category.name} value={category.name}>
+                  {category.name}
+                  <button value={category.id} onClick={(e) => handleRemove(e)}>
+                    Eliminar
+                  </button>
+                </li>
+              );
+            })}
         </div>
-      </div>     
+      </div>
+        </tbody>
+      </table>
+          <div>
+            <div>
+              <input
+                type="text"
+                value={input}
+                name="name"
+                onChange={(e) => handleChange(e)}
+              ></input>
+              <button onClick={(e) => handleSubmit(e)} type="submit">
+                Añadir categoria
+              </button>
+            </div>
+          </div>
     </div>
   );
   
