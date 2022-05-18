45ba70b63d5149d7693ed034d3ed7707ed34a579
Jose Manuel Puello Cortina <puelloj66@gmail.com>
2022-05-18 12:12:02 -0500

    graficos

--- client/src/Componentes/AdminEarnings/AdminEarningsPage.js
+++ client/src/Componentes/AdminEarnings/AdminEarningsPage.js
@@ -4,6 +4,9 @@ import NavBar from "../NavBar/NavBar"
 import Footer from './../Footer/Footer';
 import { useEffect, useState } from 'react';
 import { useDispatch , useSelector} from 'react-redux';
+import { Bar as ChartJS } from 'chart.js/auto'
+import {Bar} from 'react-chartjs-2';
+
 import { getAllReviews, deleteReview, filterByReported, 
     allUser, searchReviewById, allCourses, 
     getAllPurchases, filterPurchasesByCourse,
@@ -12,7 +15,7 @@ import { getAllReviews, deleteReview, filterByReported,
 export default function AdminSalesPage(){
 const dispatch = useDispatch()
 const allPurchases = useSelector(state => state.purchases)
-const allUsers = useSelector(state => state.reviews)
+const allUsers = useSelector(state => state.user)
 const allIds = allUsers.filter(e => e.id)
 const courses = useSelector(state => state.courses)
 // console.log(allPurchases, 'esto es all purchases')
@@ -25,11 +28,53 @@ const courses = useSelector(state => state.courses)
 
 
 const allEarnings = allPurchases.map( e => e.total_price)
-console.log(allEarnings, 'eesto es all earnings')
 const totalEarnings = allEarnings.reduce((prev, curr) => prev + curr, 0)
-console.log(totalEarnings, 'esto es total earnings')
-const finalEarnings = totalEarnings / 5
-console.log(finalEarnings, 'final earnings')
+const finalEarnings = (totalEarnings * 20) / 100
+const totalPago = ((100 - 20) / 100 ) * totalEarnings
+
+
+const Admin = allUsers.filter(t => t.roles[0].tipo == "admin");
+const totalUsers = allUsers.length;
+const totalInstructor = allUsers.filter(t => t.roles[0].tipo == "instructor");
+const totalAlumnos = allUsers.filter(t => t.roles[0].tipo == "alumno");
+const totalCourses = courses.length;
+const purchases = allPurchases.map(t => t.total_price)
+let totalPurchase = 0;
+for (let i=0; i< purchases.length; i++){
+    totalPurchase += purchases[i]
+}
+
+const user = {
+    labels: ['Admin', 'Instructor', 'Alumno'],
+    datasets:[
+        {
+            label: 'Users',
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [Admin.length, totalInstructor.length, totalAlumnos.length]
+        }
+    ]
+}
+
+const purchase = {
+    labels: ['Total Ventas', 'Admin', 'Instructor'],
+    datasets:[
+        {
+            label: 'Ventas',
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [totalPurchase, finalEarnings, totalPago]
+        }
+    ]
+}
+
+
+
+
+
+
 
     return(
     <div>
@@ -42,17 +87,38 @@ console.log(finalEarnings, 'final earnings')
          <div className={styles.insideBody}>
 
          <div className={styles.numbers}>
-            <h2>Datos totales</h2>
-            <p>Total Usuarios</p>
-            <p>Total Instructores</p>
-            <p>Total Alumnos</p>
-            <p>Total Cursos</p>
-            <p>Total Ventas</p>
-            
+         <h2>Promedio de usuarios por role</h2>          
+            <Bar
+                data={user}
+                options={{
+                    title:{
+                        display:true,
+                        text:'Promedio de usuarios por role',
+                        fontSize:20
+                      },
+                      legend:{
+                        display:true,
+                        position: 'right'
+                      }
+                }}
+            />            
          </div>
-         <div className={styles.totalEarnings}>
-            <h2>Ganancias Totales</h2>
-            <h3>$ {finalEarnings}</h3>
+         <div className={styles.numbers}>
+         <h2>Promedio de ventas y ganancias</h2>          
+            <Bar
+                data={purchase}
+                options={{
+                    title:{
+                        display:true,
+                        text:'Promedio de ventas y ganancias',
+                        fontSize:20
+                      },
+                      legend:{
+                        display:true,
+                        position: 'right'
+                      }
+                }}
+            />            
          </div>
         
         
