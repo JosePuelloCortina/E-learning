50d33533f79e1778f18bdd62a0c1c78a764dd027
albanovigna <anvignaduzzi@gmail.com>
2022-05-19 09:58:25 -0300

    base url

--- api/src/app.js
+++ api/src/app.js
@@ -8,6 +8,9 @@ const cors = require("cors");
 require("./db.js");
 
 const server = express();
+const BASE_URL = process.env.NODE_BASE_URL
+  ? process.env.NODE_BASE_URL
+  : "http://localhost:3000";
 
 server.name = "E-Learning";
 
@@ -17,7 +20,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", `${BASE_URL}`); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
